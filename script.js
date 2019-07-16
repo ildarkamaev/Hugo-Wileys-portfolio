9e82fde9ab75d6144338a0278021f588077623a4
kamaev <hello@kamaev.eu>
2019-07-16 18:36:01 +0300

    added scrollReval animation

---
+++ script.js
@@ -0,0 +1,25 @@
+window.sr = ScrollReveal();
+sr.reveal('#nav', {
+    duration: 2000,
+    origin: 'bottom'
+});
+sr.reveal('#intro', {
+    duration: 2000,
+    delay: 1000,
+    origin: 'bottom'
+});
+sr.reveal('#skills', {
+    duration: 2000,
+    origin: 'bottom',
+    viewFactor: 0.2
+});
+sr.reveal('#projects', {
+    duration: 2000,
+    origin: 'bottom',
+    viewFactor: 0.2
+});
+sr.reveal('#footer', {
+    duration: 2000,
+    origin: 'top',
+    viewFactor: 0.2
+});
